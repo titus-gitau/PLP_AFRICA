8563911c000bff8f9c0dabffbe462dd3bb23c196
titus-gitau <titus.gitau@outlook.com>
2025-08-20 12:30:57 +0300

    Week assignment submission

--- week#_assignment.py
+++ week#_assignment.py
@@ -8,13 +8,15 @@ def calculate_discount(price, discount_percentage):
         return price
 
 
+
 original_price = float(input("Enter the original price of the item: "))
 discount = float(input("Enter the discount percentage: "))
 
 
 final_price = calculate_discount(original_price, discount)
 
+
 if discount >= 20:
-    print(f"Discount applied! The final price is: {final_price:.2f}")
+    print(f"Discount has been applied! The price is: {final_price:.2f}")
 else:
-    print(f"No discount applied. The price remains: {final_price:.2f}")
+    print(f"No discount applied. The original price remains: {final_price:.2f}")
